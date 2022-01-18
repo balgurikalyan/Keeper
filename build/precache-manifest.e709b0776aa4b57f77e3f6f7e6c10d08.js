2e47b9a234e32cd90c0ca7d08c6ee59c8a21052c
balgurikalyan <balgurikalyan711@gmail.com>
2022-01-18 21:47:12 +0530

    production

---
+++ build/precache-manifest.e709b0776aa4b57f77e3f6f7e6c10d08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd10c2d83186ae4b3dc4b34074721c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "fd50d28b90c1a7225d85",
+    "url": "/static/js/main.8fb407e3.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
